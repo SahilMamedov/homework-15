7bfa13594a4dabce38a1f055fca141c8a81f525b
SahilMamedov <sahilim@code.edu.az>
2022-04-06 17:48:48 +0400

    Refactor:

---
+++ UtilsLibrary/EXceptions/CapacityLimitException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_15
+{
+   public class CapacityLimitException:Exception
+    {
+        public CapacityLimitException(string message):base(message)
+        {
+
+        }
+    }
+}
